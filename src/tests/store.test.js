369d167817c4440a88afcb988b8c7770a72a9766
John Murianki <mcjthiongo@gmail.com>
2023-08-04 09:18:20 +0300

    Fix linter error

--- src/tests/store.test.js
+++ src/tests/store.test.js
@@ -4,7 +4,7 @@ import fetchCountries from '../redux/countries/fetchCountries';
 describe('test the store', () => {
   it('check if the store initial State is empty', () => {
     const initialState = store.getState().countries;
-    expect(initialState).toEqual({ countries: [], error: null, isLoading:false });
+    expect(initialState).toEqual({ countries: [], error: null, isLoading: false });
   });
 
   it('check if the store initial State is fill with data after dispatch', async () => {
